7132fb86b496829ccaaa7309596cb84003112d7d
piglercs <piglercs@starschema.net>
2020-02-22 21:41:22 +0100

    added flatlist

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import { StyleSheet, TextInput, Button, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, Button, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals,
+      {uid: Math.random().toString(), value: enteredGoal}
+    ])
+    setEnteredGoal('')
+  }
+
+  const goalInputHandler = (goal) => {
+    setEnteredGoal(goal)
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput placeholder='Course goal'
-          style={styles.input} />
-        <Button title='Add' />
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal} />
+        <Button title='Add' onPress={addGoalHandler} />
       </View>
-      <View>
-      </View>
+      <FlatList 
+        keyExtractor={(item, index) => item.uid}
+        data={courseGoals} 
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )} />
+
     </View>
   );
 }
@@ -30,4 +54,11 @@ const styles = StyleSheet.create({
     padding: 10,
     width: '80%'
   },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
